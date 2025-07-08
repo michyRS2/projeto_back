a5167818942f7622088ef093c8611eaf2eec65ab
Miguel Rodrigues <miguel11rodrigues@gmail.com>
2025-07-08 14:42:06 +0100

    Configuração da bd

--- config/database.js
+++ config/database.js
@@ -1,12 +1,14 @@
-var Sequelize = require('sequelize');
-const sequelize = new Sequelize(
-    'projeto_ai',
-    'postgres',
-    'sql123',
-    {
-        host: 'localhost',
-        port: '5432',
-        dialect: 'postgres'
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
     }
-);
+  }
+});
+
 module.exports = sequelize;
