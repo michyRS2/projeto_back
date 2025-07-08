92ec629221dec7fb4d26ee4ef6f0f08aecf9820a
Miguel Rodrigues <miguel11rodrigues@gmail.com>
2025-07-08 19:50:37 +0100

    app com rota correta

--- app.js
+++ app.js
@@ -21,7 +21,11 @@ const moduloRoutes = require('./routes/moduloRoutes');
 
 //Configurações
 app.set("port", process.env.PORT || 3000);
-app.use(cors({origin: 'http://localhost:5173', credentials: true}));
+app.use(cors({
+  origin: ['https://projeto-front-yg80.onrender.com'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'], 
+  credentials: true}));
 
 //Middlewares
 app.use(cookieParser());
